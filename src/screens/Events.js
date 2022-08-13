bfc2b932be93610b2eb7b52e96626cb63a869801
ql420 <94789931+ql420@users.noreply.github.com>
2022-08-13 21:12:39 +0100

    incomplete event page

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -3,12 +3,16 @@ import { Text, Image, Button, View, ScrollView, Alert, Modal, StyleSheet, Pressa
 import Card from '../Card';
 import Header from '../Header';
 import { Calendar, CalendarList, Agenda, LocaleConfig } from 'react-native-calendars';
+import { SearchBar } from 'react-native-elements';
 
 export default function Events() {
-    return (
+
+        return (
         <ScrollView showsVerticalScrollIndicator={false} style={{ flexShrink: 0 }}>
-            <View style = {{ paddingHorizontal: 20, paddingVertical: 20, justifyContent: 'center', flexDirection: "row", fontFamily: 'OpenSans_700Bold', fontSize: 10, backgroundColor: 'transparent',}}>
-                <Text>:)</Text>
+            
+            <View style = {{ width:350, paddingVertical: 20, justifyContent: 'center',  fontFamily: 'OpenSans_700Bold', fontSize: 10, backgroundColor: 'transparent',}}>
+                <Card img={require('./../../assets/banner.jpeg')} title="Event Name" desc="Event Description" height={200} time="Mon, Jan 1, 00:00 - 24:00" />
+                <Card img={require('./../../assets/banner.jpeg')} title="Event Name" desc="Event Description" height={200} time="Mon, Jan 1, 00:00 - 24:00" />
             </View>
         </ScrollView>
         
