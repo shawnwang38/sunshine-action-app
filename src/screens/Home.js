6d8b9d3c8ff5b28fb808c173dc3cca964ff52c96
prakhars03 <107943143+prakhars03@users.noreply.github.com>
2022-08-24 22:35:49 +0530

    About Us added to Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -19,6 +19,10 @@ export default function Home() {
                 <Card img={require('./../../assets/banner.jpeg')} title="Event Name" height={125} time="Mon, Jan 1" style = {{ width: 150, marginRight: 20 }} />
                 <Card img={require('./../../assets/banner.jpeg')} title="Event Name" height={125} time="Mon, Jan 1" style = {{ width: 150 }} />
             </ScrollView>
+            <Text style={{ fontFamily: 'OpenSans_700Bold', fontSize: 24, alignSelf: 'flex-start', marginBottom: 8, color: 'black', paddingLeft: 20 }}>About Us</Text>
+                <Text style={{ fontFamily: 'OpenSans_700Bold', fontSize: 16, alignSelf: 'flex-start', marginBottom: 8, color: 'grey', paddingLeft: 20, paddingBottom: 30 }}>
+                Sunshine Action is a non-profit organisation founded in Hong Kong in 2008. Since our establishment, the charity has partnered with 642 local centres and organisations across 20 countries including the USA, Chile, and Vietnam, helping over 340,000 individuals and families. Our programs span 14 of the UN Sustainable Development Goals, mainly focusing on food and hunger. We are non-religious, non-political and non-discriminatory, and are also 100% based on volunteers which means that 95% of funding goes directly towards those in need.
+                </Text>
         </ScrollView>
     );
 }
