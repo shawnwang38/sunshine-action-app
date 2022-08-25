3155ea4b735907df0982d04cdc3caf87f5b995e2
Rachel Kovach-Fuentes <rkovachfuentes@uchicago.edu>
2022-08-24 23:19:46 -0400

    bug fix

--- src/screens/AccountSettings.js
+++ src/screens/AccountSettings.js
@@ -10,13 +10,13 @@ export default function AccountSettings({ navigation, route }) {
     return (
         <ScrollView showsVerticalScrollIndicator={false} style={{ flexShrink: 0 }}>
             <View style = {{ paddingHorizontal: 20, alignItems: 'center', paddingVertical: 20 }}>
-                <AccountModal title="Edit Username" field="Username" />
+                <AccountModal title="Edit Username" field="Username" text="Enter new username:" />
                 <View style={styles.space} />
-                <AccountModal title="Edit Password" field="Password" />
+                <AccountModal title="Edit Password" field="Password" text="Enter new password:" />
                 <View style={styles.space} />
-                <AccountModal title="Edit Location" field="City, State/Province, Country" />
+                <AccountModal title="Edit Location" field="City, State/Province, Country" text="Enter City, State/Province, Country" />
                 <View style={styles.space} />
-                <AccountModal title="Edit Email Address" field="email@email.com" />
+                <AccountModal title="Edit Email Address" field="email@email.com" text="Enter new email address:" />
                 <View style={styles.space} />
                 <AccountModal title="Notification Preferences" />
                 
